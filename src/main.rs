26df12652b5dfa832a9bfc8840d85f4e1d3d5df1
Anton <antoni.piasecki2021p@zsepoznan.pl>
2024-11-02 16:53:56 +0100

    struct

--- src/main.rs
+++ src/main.rs
@@ -5,107 +5,128 @@ use std::{
     io::{self, Write},
 };
 
-fn read_line() -> Result<String, io::Error> {
-    let mut line = String::new();
-    io::stdin().read_line(&mut line)?;
-    Ok(line.trim().to_owned())
-}
-
-fn get_buffer_size(buffer: &[String]) -> usize {
-    buffer.iter().map(|x| x.len()).sum()
-}
-
-fn open_file(
+struct Editor {
     path: String,
-    buffer: &mut Vec<String>,
-    current: &mut usize,
-) -> Result<(), Box<dyn Error>> {
-    *buffer = fs::read_to_string(path)?
-        .trim()
-        .lines()
-        .map(str::to_owned)
-        .collect();
-    if !buffer.is_empty() {
-        *current = buffer.len() - 1;
-    }
-    println!("{}", get_buffer_size(buffer));
-    Ok(())
+    buffer: Vec<String>,
+    current: usize,
 }
 
-fn write_file(path: String, buffer: &[String]) -> Result<(), Box<dyn Error>> {
-    let mut file = fs::File::create(path)?;
-    file.write_all(buffer.join("\n").as_bytes())?;
-    println!("{}", get_buffer_size(buffer));
-    Ok(())
+impl Editor {
+    pub fn new() -> Editor {
+        Editor {
+            path: String::new(),
+            buffer: vec![],
+            current: 0,
+        }
+    }
+
+    pub fn main_loop(&mut self) -> Result<(), Box<dyn Error>> {
+        loop {
+            let line = self.read_line()?;
+            if line.is_empty() {
+                continue;
+            }
+
+            match line.chars().next().unwrap() {
+                // append
+                'a' if line.len() == 1 => loop {
+                    let line = self.read_line()?;
+                    if line == "." {
+                        break;
+                    }
+                    self.current += 1;
+                    self.buffer.insert(self.current, line);
+                },
+                // open file
+                'e' if line.starts_with("e ") => {
+                    self.path = line[2..].into();
+                    self.open_file(self.path.clone())?;
+                }
+                // print line number
+                'n' if line.len() == 1 => {
+                    print!("{}\t", self.current + 1);
+                    self.print_current_line();
+                }
+                // print line
+                'p' | '.' if line.len() == 1 => {
+                    self.print_current_line();
+                }
+                // exit
+                'q' if line.len() == 1 => break,
+                // save file
+                'w' => {
+                    self.write_file(self.path.clone())?;
+                }
+                // select last line and print it
+                '$' if line.len() == 1 => {
+                    self.current = self.buffer.len() - 1;
+                    self.print_current_line();
+                }
+                // select line and print it
+                '0'..='9' => {
+                    let n = line.trim().parse::<usize>().unwrap() - 1;
+                    match self.buffer.get(n) {
+                        Some(_) => {
+                            self.current = n;
+                            self.print_current_line();
+                        }
+                        None => println!("?"),
+                    }
+                }
+                _ => {
+                    println!("?")
+                }
+            }
+        }
+        Ok(())
+    }
+
+    pub fn open_file(&mut self, path: String) -> Result<(), Box<dyn Error>> {
+        self.path.clone_from(&path);
+
+        self.buffer = fs::read_to_string(path)?
+            .trim()
+            .lines()
+            .map(str::to_owned)
+            .collect();
+        if !self.buffer.is_empty() {
+            self.current = self.buffer.len() - 1;
+        }
+        self.print_buffer_size();
+        Ok(())
+    }
+
+    fn write_file(&self, path: String) -> Result<(), Box<dyn Error>> {
+        let mut file = fs::File::create(path)?;
+        file.write_all(self.buffer.join("\n").as_bytes())?;
+        self.print_buffer_size();
+        Ok(())
+    }
+
+    fn read_line(&self) -> Result<String, io::Error> {
+        let mut line = String::new();
+        io::stdin().read_line(&mut line)?;
+        Ok(line.trim().to_owned())
+    }
+
+    fn print_current_line(&self) {
+        println!("{}", self.buffer.get(self.current).unwrap());
+    }
+
+    fn print_buffer_size(&self) {
+        println!("{}", self.buffer.iter().map(|x| x.len()).sum::<usize>());
+    }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let mut editor = Editor::new();
+
     let mut args = env::args();
     _ = args.next();
-
-    let mut path = String::new();
-    let mut buffer: Vec<String> = vec![];
-    let mut current = 0;
-
-    if let Some(p) = args.next() {
-        path = p;
-        open_file(path.clone(), &mut buffer, &mut current)?;
+    if let Some(path) = args.next() {
+        editor.open_file(path.clone())?;
     }
 
-    loop {
-        let line = read_line()?;
-        if line.is_empty() {
-            continue;
-        }
-
-        match line.chars().next().unwrap() {
-            // append
-            'a' if line.len() == 1 => loop {
-                let line = read_line()?;
-                if line == "." {
-                    break;
-                }
-                current += 1;
-                buffer.insert(current, line);
-            },
-            // print line number
-            'n' if line.len() == 1 => {
-                println!("{}\t{}", current + 1, buffer.get(current).unwrap());
-            }
-            // print line
-            'p' | '.' if line.len() == 1 => {
-                println!("{}", buffer.get(current).unwrap());
-            }
-            // exit
-            'q' if line.len() == 1 => break,
-            // select last line and print it
-            '$' if line.len() == 1 => {
-                current = buffer.len() - 1;
-                println!("{}", buffer.get(current).unwrap());
-            }
-            'e' if line.starts_with("e ") => {
-                path = line[2..].into();
-                open_file(path.clone(), &mut buffer, &mut current)?;
-            }
-            'w' => {
-                write_file(path.clone(), &buffer)?;
-            }
-            // select line and print it
-            '0'..='9' => {
-                let n = line.trim().parse::<usize>().unwrap() - 1;
-                match buffer.get(n) {
-                    Some(line) => {
-                        current = n;
-                        println!("{}", line);
-                    }
-                    None => println!("?"),
-                }
-            }
-            _ => {
-                println!("?")
-            }
-        }
-    }
-
+    editor.main_loop()?;
     Ok(())
 }
